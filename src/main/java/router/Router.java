0affb4bd1a950de9e898597bf6fae2a97d35eb1c
daviaprea <apredav@gmail.com>
2026-04-23 11:52:27 +0200

    exception handling

--- src/main/java/router/Router.java
+++ src/main/java/router/Router.java
@@ -3,6 +3,8 @@ package router;
 import model.Request;
 import model.Response;
 
+import java.util.Optional;
+
 public class Router {
     private final Segment root = Segment.withDefault();
 
@@ -13,9 +15,17 @@ public class Router {
         for (String segmentName : path) {
             Segment segment = Segment.withDefault();
 
+            if (currSegment.children().containsKey(segmentName)) {
+                throw new IllegalStateException();
+            }
+
             currSegment.children().put(segmentName, segment);
 
             currSegment = segment;
+        }
+
+        if (currSegment.methodHandlers().containsKey(requestHandler.getMethod())) {
+            throw new IllegalStateException();
         }
 
         currSegment.methodHandlers().put(requestHandler.getMethod(), requestHandler);
@@ -29,6 +39,10 @@ public class Router {
             currSegment = root.children().get(segmentName);
         }
 
-        return currSegment.methodHandlers().get(request.method()).handle(request);
+        RequestHandler handler = Optional
+                .ofNullable(currSegment.methodHandlers().get(request.method()))
+                .orElseThrow();
+
+        return handler.handle(request);
     }
 }
